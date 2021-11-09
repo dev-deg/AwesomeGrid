e3143bbaba3e0235d15b2c7e127c49cd6779b9f7
david <david.deguara@mcast.edu.mt>
2021-11-09 11:43:28 +0100

    Grid is clickable and functional

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -6,9 +6,23 @@ public class GridController : MonoBehaviour
 {
     public GameObject text;
     public GameObject canvas;
+    public Camera cam;
+
+    private GridSystem gridSys;
+
     // Start is called before the first frame update
     void Start()
     {
-        GridSystem gridSys = new GridSystem(4, 4, 1f,text,canvas);
+        gridSys = new GridSystem(10, 8, 1f, text, canvas, new Vector3(-5, -4));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Something happens
+            Vector3 worldMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            gridSys.setValue(worldMousePosition, "1");
+        }
     }
 }
--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class GridSystem
 {
     private int _width; //Number of boxes wide
     private int _height; //Number of boxes high
     private float _cellSize; //Size of each cell
-    private int[,] _gridArray; //2D Array
+    private int[,] _gridArray; //2D Array of Coords
+    private Text[,] _textArray; //2D Array of the Text GOs
+    private Vector3 _originLocation; //The start point of the grid
 
-    public GridSystem(int width, int height, float cellSize, GameObject text, GameObject canvas)
+    public GridSystem(int width, int height, float cellSize,
+        GameObject text, GameObject canvas, Vector3 origin)
+
     {
         this._width = width;
         this._height = height;
         this._cellSize = cellSize;
         this._gridArray = new int[width, height];
-
+        this._textArray = new Text[width, height];
+        this._originLocation = origin;
+        
         //Looping through all the coords
         for (int x = 0; x < _gridArray.GetLength(0); x++)
         {
@@ -25,19 +32,62 @@ public class GridSystem
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1 , y), Color.white, 100f);
 
-                //Create a Text
+                //Create a Text object
                 GameObject newGO = GameObject.Instantiate(text, GetWorldPosition(x, y) + (new Vector3(_cellSize,_cellSize) * 0.5f), Quaternion.identity, canvas.transform);
                 newGO.name = "Text" + x + y;
+                _textArray[x, y] = newGO.GetComponent<Text>();
             }
         }
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
+    //Gets the value of a specific text object
+    public void setValue(int x, int y, string value)
+    {
+        if ((x >= 0 && x < _width) && (y >= 0 && y < _height))
+        {
+            _textArray[x, y].text = value;
+        }
+        else
+        {
+            Debug.LogError("Out of range! What are you doing?!");
+        }
+    }
+
+    //Overloading SetValue
+    public void setValue(Vector3 worldPosition, string value)
+    {
+        Vector2Int pos = getGridCoordinates(worldPosition);
+        setValue(pos.x, pos.y, value);
+    }
+
+    //Sets the value of a specific text object
+    public string getValue(int x, int y)
+    {
+
+        if ((x >= 0 && x < _width) && (y >= 0 && y < _height))
+        {
+            return _textArray[x, y].text;
+        }
+        else
+        {
+            return "-1";
+        }
+            
+    }
+
     //Converts x and y coords to global positioning
     private Vector3 GetWorldPosition(int x, int y)
     {
-        return new Vector3(x,y) * _cellSize;
+        return new Vector3(x,y) * _cellSize + _originLocation;
+    }
+
+    private Vector2Int getGridCoordinates(Vector3 worldPosition)
+    {
+        int x = Mathf.FloorToInt((worldPosition - _originLocation).x / _cellSize);
+        int y = Mathf.FloorToInt((worldPosition - _originLocation).y / _cellSize);
+        return new Vector2Int(x, y);
     }
 
 }
