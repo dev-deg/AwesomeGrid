2c26a918079fb647a12ad521607a71c379c74f73
david <david.deguara@mcast.edu.mt>
2021-11-11 10:58:49 +0100

    grid working properly

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
+    [SerializeField] private GameObject text; //Reference to our text prefab
+    [SerializeField] private Canvas canvas; //Reference to the canvas (as a parent)
+
+
     private GridSystem _gridSystem;
 
     void Start()
     {
-        _gridSystem = new GridSystem(4, 4, 1f, new Vector3(-3, -3));
+        //Instantiating our grid system
+        _gridSystem = new GridSystem(10, 10, 1f, new Vector3(-5, -5),text,canvas);
+        //To test our new functions
+        _gridSystem.setValue(new Vector2Int(4, 4), "25");
+        Debug.Log(_gridSystem.getValue(new Vector2Int(2, 2)));
     }
 }
--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GridSystem
 {
     private int _width; //Number of boxes wide
     private int _height; //Number of boxes high
     private float _boxSize; //Size of each box
     private int[,] _boxArray; //2D Array storing all the boxes
+    private Text[,] _textarray; //2D Array storing all the text boxes
+
     private Vector3 _origin; //World location where the grid will start
 
-    public GridSystem(int width, int height, float boxSize, Vector3 origin)
+    public GridSystem(int width, int height, float boxSize, Vector3 origin, GameObject text, Canvas canvas)
     {
         this._width = width;
         this._height = height;
@@ -18,6 +20,7 @@ public class GridSystem
         this._origin = origin;
 
         this._boxArray = new int[width,height];
+        this._textarray = new Text[width, height];
 
         for (int x = 0; x < _boxArray.GetLength(0); x++)
         {
@@ -25,15 +28,48 @@ public class GridSystem
             {
                 Debug.DrawLine(GetWorldPosition(new Vector2Int(x, y)), GetWorldPosition(new Vector2Int(x + 1, y)),Color.white,100f);
                 Debug.DrawLine(GetWorldPosition(new Vector2Int(x, y)), GetWorldPosition(new Vector2Int(x, y + 1)), Color.white,100f);
+
+                //Instantiate a text object
+                GameObject newGO = GameObject.Instantiate(text, GetWorldPosition(new Vector2Int(x, y)) + (new Vector3(_boxSize,_boxSize) * 0.5f),
+                    Quaternion.identity, canvas.transform);
+                newGO.name = "Text" + x + y;
+                _textarray[x, y] = newGO.GetComponent<Text>();
             }
         }
         //Add the two missing lines
-
+        Debug.DrawLine(GetWorldPosition(new Vector2Int(0, height)), GetWorldPosition(new Vector2Int(width, height)), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(new Vector2Int(width, 0)), GetWorldPosition(new Vector2Int(width, height)), Color.white, 100f);
     }
 
     private Vector3 GetWorldPosition(Vector2Int coords)
     {
         return new Vector3(coords.x,coords.y) * _boxSize + _origin ;
+    }
+
+    public void setValue(Vector2Int coords, string value)
+    {
+        if ((coords.x >= 0 && coords.x < _width) &&
+            (coords.y >=0 && coords.y < _height))
+        {
+            _textarray[coords.x, coords.y].text = value;
+        }
+        else
+        {
+            Debug.LogError("Out of range! What are you doing?!");
+        }
+    }
+
+    public string getValue(Vector2Int coords)
+    {
+        if ((coords.x >= 0 && coords.x < _width) &&
+            (coords.y >= 0 && coords.y < _height))
+        {
+           return _textarray[coords.x, coords.y].text;
+        }
+        else
+        {
+            return "-1";
+        }
     }
 
 }
